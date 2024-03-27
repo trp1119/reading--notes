653815a68d2689025d3f1a0474394d5c1f753c3a
tangruipeng <tang@trp1119.com>
2024-03-28 00:38:59 +0800

    feat: 新增双端 Diff 算法

---
+++ Vue.js 设计与实现/10 双端 Diff 算法/10.5 移除不存在的元素.js
@@ -0,0 +1,280 @@
+/**
+ * 10.5 移除不存在的元素
+ */
+const Fragment = Symbol()
+const vnode_fragment = {
+  type: Fragment,
+  children: [
+    { type: 'li', children: 'text1' },
+    { type: 'li', children: 'text2' },
+    { type: 'li', children: 'text3' }
+  ]
+}
+
+function normalizeClass (c) {
+  // TODO
+  return c
+}
+
+function shouldSetAsProps  (el, key) {
+  if (key === 'form' && el.tagName === 'INPUT') return false
+  return key in el
+}
+
+function createRenderer(options) {
+  const {
+    createElement,
+    insert,
+    setElementText,
+    patchProps,
+    unmount,
+    createText,
+    setText
+  } = options
+
+  function mountElement(vnode, container, anchor) { // 
+    const el = vnode.el = createElement(vnode.type)
+
+    if (typeof vnode.children === 'string') {
+      setElementText(el, vnode.children)
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach(child => {
+        patch(null, child, el)
+      })
+    }
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key])
+      }
+    }
+
+    insert(el, container, anchor)
+  }
+
+  function patchChildren(n1, n2, container) { // n1 旧 vnode n2 新 vnode
+    if (typeof n2.children === 'string') {
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c))
+      }
+      setElementText(container, n2.children)
+    } else if (Array.isArray(n2.children)) {
+      if (Array.isArray(n1.children)) { // n1 n2 都是 array，需进行对比，diff 算法
+        // 双端 Diff 算法
+        patchKeyedChildren(n1, n2, container)
+      } else {
+        setElementText(container, '') // 清空
+        n2.children.forEach(c => patch(null, c, container))
+      }
+    } else {
+      // 新子节点不存在
+      if (Array.isArray(n1.children)) {
+        n1.children.forEach(c => unmount(c))
+      } else if (typeof n1.children === 'string') {
+        setElementText(container, '')
+      }
+    }
+  }
+
+  function patchKeyedChildren (n1, n2, container) {
+    const oldChildren = n1.children
+    const newChildren = n2.children
+    // 四个索引值
+    let oldStartIdx = 0
+    let oldEndIdx = oldChildren.length - 1
+    let newStartIdx = 0
+    let newEndIdx = newChildren.length - 1
+    // 四个索引值指向的 vnode 节点
+    let oldStartVNode = oldChildren[oldStartIdx]
+    let oldEndVNode = oldChildren[oldEndIdx]
+    let newStartVNode = newChildren[newStartIdx]
+    let newEndVNode = newChildren[newEndIdx]
+
+    while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+      if (!oldStartVNode) { // 已经处理过
+        oldStartVNode = oldChildren[++oldStartIdx]
+      } else if (!oldEndVNode) {
+        oldEndVNode = oldChildren[--oldEndIdx]
+      } else if (oldStartVNode.key === newStartVNode.key) {
+        patch(oldStartVNode, newStartVNode, container)
+        // 均位于头部，不需要移动
+        oldStartVNode = oldChildren[++oldStartIdx]
+        newStartVNode = newChildren[++newStartIdx]
+      } else if (oldEndVNode.key === newEndVNode.key) {
+        patch(oldEndVNode, newEndVNode, container)
+        // 均处于尾部，不需要移动
+        oldEndVNode = oldChildren[--oldEndIdx]
+        newEndIdx = newChildren[--newEndIdx]
+      } else if (oldStartVNode.key === newEndVNode.key) {
+        patch(oldStartVNode, newEndVNode, container)
+        insert(oldStartVNode.el, container, oldEndVNode.el.nextSibling)
+        oldStartVNode = oldChildren[++oldStartIdx]
+        newEndVNode = newChildren[--newEndIdx]
+      } else if (oldEndVNode.key === newStartVNode.key) {
+        // 打补丁
+        patch(oldEndVNode, newStartVNode, container)
+        // 换位置
+        insert(oldEndVNode.el, container, oldStartVNode.el)
+        // 移动完 DOM，更新索引值，并指向下一个位置
+        oldEndVNode = oldChildren[--oldEndIdx]
+        newStartVNode = newChildren[++newStartIdx]
+      } else {
+        const idxInOld = oldChildren.findIndex(oldVNode => oldVNode.key === newStartVNode.key) // 寻找与 newStartVNode 相同 key 的元素
+        if (idxInOld > 0) {
+          const vNodeToMove = oldChildren[idxInOld]
+          patch(vNodeToMove, newStartVNode, container)
+          insert(vNodeToMove.el, container, oldStartVNode.el)
+          oldChildren[idxInOld] = undefined // 已处理过，设置为 undefined，以便后续跳过
+        } else {
+          patch(null, newStartVNode, container, oldStartVNode.el)
+        }
+        newStartVNode = newChildren[++newStartIdx] // 已根据 newStartVNode 换过位置/新增，处理下一位置
+      }
+    }
+
+    //  while 循环结束，检查索引值情况
+    if (oldEndIdx < oldStartIdx && newStartIdx <= newEndIdx) { // 新子节点中有未挂载情况
+      for (let i = newStartIdx; i <= newEndIdx; i++) {
+        patch(null, newChildren[i], container, oldStartVNode.el)
+      }
+    } else if (newEndIdx < newStartIdx && oldStartIdx <= oldEndIdx) {
+      for (let i = oldStartIdx; i <= oldEndIdx; i++) {
+        unmount(oldChildren[i])
+      }
+    }
+  }
+
+  function patchElement(n1, n2) {
+    const el = n2.el = n1.el
+    const oldProps = n1.props
+    const newProps = n2.props
+    for (const key in newProps) {
+      if (newProps[key] !== oldProps[key]) {
+        patchProps(el, key, oldProps[key], newProps[key])
+      }
+    }
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        patchProps(el, key, oldProps[key], null)
+      }
+    }
+    patchChildren(n1, n2, el)
+  }
+
+  function patch (n1, n2, container, anchor) { // 增加第四个参数，锚点元素
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1)
+      n1 = null
+    }
+    const { type } = n2
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container, anchor)
+      } else {
+        patchElement(n1, n2)
+      }
+    } else if (type === Text) {
+      if (!n1) {
+        const el = n2.el = createText(n2.children)
+        insert(el, container)
+      } else {
+        const el = n2.el = n1.el
+        if (n2.children !== n1.children) {
+          setText(el, n2.children)
+        }
+      }
+    } else if (type === Fragment) {
+      if (!n1) {
+        n2.children.forEach(c => patch(null, c, container))
+      } else {
+        patchChildren(n1, n2, container)
+      }
+    } else if (typeof type === 'object') {
+    } else if (type === 'xxx') {
+    }
+  }
+
+  function render (vnode, container) {
+    if (vnode) {
+      patch(container._vnode, vnode, container)
+    } else {
+      if (container._vnode) {
+        unmount(container._vnode)
+      }
+    }
+    container._vnode = vnode
+  }
+
+  return {
+    render
+  }
+}
+
+const renderer = createRenderer({
+  createElement (tag) {
+    return document.createElement(tag)
+  },
+  setElementText (el, text) {
+    el.textContent = text
+  },
+  insert (el, parent, anchor = null) {
+    parent.insertBefore(el, anchor)
+  },
+  createText(text) {
+    return document.createTextNode(text)
+  },
+  setText(el, text) {
+    el.nodeValue = text
+  },
+  patchProps (el, key, preValue, nextValue) {
+    if (/^on/.test(key)) {
+      const name = key.slice(2).toLowerCase()
+
+      const invokers = el._vei || (el._vei = {})
+      let invoker = invokers[key]
+
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            console.log('执行', el, e.timeStamp)
+            if (e.timeStamp < invoker.attched) return
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach(fn => fn(e))
+            } else {
+              invoker.value(e)
+            }
+          }
+          invoker.value = nextValue
+          invoker.attched = performance.now()
+          console.log('绑定', el, invoker.attched)
+          el.addEventListener(name, invoker)
+        } else {
+          invoker.value = nextValue
+        }
+      } else if (invoker) {
+        el.removeEventListener(name, invoker)
+      }
+    } else if (key === 'class') {
+      el.className = normalizeClass(nextValue) || ''
+    } else if (shouldSetAsProps(el, key)) {
+      const type = typeof el[key]
+      if (type === 'boolean' && nextValue === '') {
+        el[key] = true
+      } else {
+        el[key] = nextValue
+      }
+    } else {
+      el.setAttribute(key, nextValue)
+    }
+  },
+  unmount (vnode) {
+    const el = vnode.el
+    if (vnode.type === Fragment) {
+      vnode.children.forEach(c => unmount(c))
+    }
+    const parent = el.parentNode
+    if (parent) {
+      parent.removeChild(el)
+    }
+  }
+})
